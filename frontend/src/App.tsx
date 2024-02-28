269b4993871480440ff95044ce9b4763a45a9844
sudhanshu3430 <127764778+sudhanshu3430@users.noreply.github.com>
2024-02-28 17:17:01 +0530

    edit hotel feature added

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -10,6 +10,7 @@ import SignIn from "./pages/SignIn";
 import AddHotel from './pages/AddHotel';
 import { useAppContext } from "./contexts/AppContext";
 import MyHotels from "./pages/MyHotels";
+import EditHotel from "./pages/EditHotel";
 
 
 const App = () => {
@@ -38,6 +39,13 @@ const App = () => {
           <Route path = "/my-hotels" element={
           <Layout>
             <MyHotels/>
+          </Layout>
+          
+          
+        }/>
+          <Route path = "/edit-hotel/:hotelId" element={
+          <Layout>
+            <EditHotel/>
           </Layout>
           
           
--- frontend/src/api-client.ts
+++ frontend/src/api-client.ts
@@ -2,6 +2,7 @@ import { RegisterFormData } from "./pages/Register";
 import { SignInFormData } from './pages/SignIn';
 import { HotelType } from '../../backend/src/shared/types';
 
+
 const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || '';
 
 export const register = async(formdata: RegisterFormData) =>  {
@@ -87,3 +88,30 @@ export const fetchMyHotels = async(): Promise<HotelType[]> =>{
     }
     return response.json();
 }
+
+export const fetchMyHotelById = async (hotelId: string): Promise<HotelType>=>{
+    const response = await fetch(`${API_BASE_URL}/api/my-hotels/${hotelId}`, {
+        credentials:"include",
+
+    })
+    if(!response.ok){
+        throw new Error("Error Fetching new Hotels");
+    }
+    return response.json();
+
+}
+
+export const updateMyHotelById = async (hotelFormData: FormData) =>{
+    const response = await fetch (`${API_BASE_URL}/api/my-hotels/${hotelFormData.get('hotelId')}`,{
+        method: "PUT",
+        body: hotelFormData,
+        credentials:"include"
+    });
+    if(!response.ok){
+        throw new Error("Failed to update Hotel")
+
+    
+    }
+    return response.json();
+}
+
