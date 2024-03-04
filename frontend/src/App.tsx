ddaf778342916ef88b348cffb91c05b2e2f42069
sudhanshu3430 <127764778+sudhanshu3430@users.noreply.github.com>
2024-03-04 16:37:49 +0530

    my bookings feature added

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -14,6 +14,7 @@ import EditHotel from "./pages/EditHotel";
 import Search from "./pages/Search";
 import Detail from "./pages/Detail";
 import Booking from "./pages/Booking";
+import MyBookings from "./pages/MyBookings";
 
 const App = () => {
   const { isLoggedIn } = useAppContext();
@@ -84,6 +85,14 @@ const App = () => {
               element={
                 <Layout>
                   <MyHotels />
+                </Layout>
+              }
+            />
+             <Route
+              path="/my-bookings"
+              element={
+                <Layout>
+                  <MyBookings />
                 </Layout>
               }
             />
